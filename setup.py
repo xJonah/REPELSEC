8aa2af74b6808a5b17d8e77634a1f66532e256d9
Jonah Reader <200089025@aston.ac.uk>
2024-04-18 18:41:44 +0100

    V0.6

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="repelsec",
-    version="0.5",
+    version="0.6",
     packages=find_packages(),
     include_package_data=True,
     install_requires=read_requirements(),
